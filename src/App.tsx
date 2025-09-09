33e39a20883ca284d8ebf4c0f9fce9ab65f0bb6b
dr1602 <david.arangelg@gmail.com>
2025-09-09 16:32:39 -0600

    refactor: moves the components to an accordion to have a better visibility of future components

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,32 @@ import { Grid } from './components/Grid';
 function App() {
   return (
     <div>
-      <Practice />
-      <Accordion />
-      <Buttons />
-      <Checks />
-      <Containers />
-      <Grid />
+      <div className='accordion accordion-flush' id='accordionFlushExample'>
+        <div className='accordion-item'>
+          <button
+            className='accordion-button collapsed'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#flush-collapseOne'
+            aria-expanded='false'
+            aria-controls='flush-collapseOne'
+          >
+            Several Examples
+          </button>
+        </div>
+        <div
+          id='flush-collapseOne'
+          className='accordion-collapse collapse'
+          aria-labelledby='flush-headingOne'
+        >
+          <Practice />
+          <Accordion />
+          <Buttons />
+          <Checks />
+          <Containers />
+          <Grid />
+        </div>
+      </div>
     </div>
   );
 }
